7823fc302917253108e20d31df20674b9e9b1e67
sharkofecuis <sharkofecuis@gmail.com>
2016-12-30 01:56:12 -0800

    install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
  resources :groups
 root 'grouptes#index'
+
+
 end
